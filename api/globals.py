6d57607838c1fa2b8c8abab809029f0f224bba94
MorelK1 <kouhossounon.vianney@gmail.com>
2023-05-13 22:13:13 +0100

    `feat` add bcrypt to global variable

--- api/globals.py
+++ api/globals.py
@@ -1,22 +1,27 @@
 import flask.app
 import flask_pymongo
+from flask_bcrypt import Bcrypt
 from flask_pymongo import PyMongo
 from dotenv import load_dotenv, find_dotenv
 from logger.MongoLogger import MongoLogger
 
-def setup_db(app: flask.app.Flask)-> tuple[flask_pymongo.wrappers.Database, flask_pymongo.wrappers.MongoClient]:
+
+def setup_db(app: flask.app.Flask) -> tuple[flask_pymongo.wrappers.Database, flask_pymongo.wrappers.MongoClient]:
     # function to connect to MongoDB
     global db, client
-    client= PyMongo(app)
-    db= client.db
-    client= client.cx
+    client = PyMongo(app)
+    db = client.db
+    client = client.cx
     return db, client
+
 
 def setup_log(app: flask.app.Flask, db: flask_pymongo.wrappers.Database, collection_name: str) -> MongoLogger:
     global logger
     logger = MongoLogger('Logger', db, collection_name)
     return logger
 
-#load environment variables
+
+# load environment variables
 load_dotenv(find_dotenv('.env'))
 db, client = None, PyMongo()
+bcrypt = Bcrypt()
