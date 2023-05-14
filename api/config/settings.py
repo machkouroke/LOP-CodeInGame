0469c50c08d2586001d1e78e76d075265dad7da9
MorelK1 <kouhossounon.vianney@gmail.com>
2023-05-14 11:44:34 +0100

    `feat` add endpoint and route to setting

--- api/config/settings.py
+++ api/config/settings.py
@@ -2,12 +2,17 @@ import os
 
 from dotenv import load_dotenv, find_dotenv
 
+from api.config.routes import USER_ROUTE
+from api.config.views import USER_VIEW
+
 load_dotenv(find_dotenv('.env'))
-DB_NAME=os.environ.get("DB_NAME")
-DB_USER= os.environ.get("MONGO_USERNAME")
-DB_PASSWORD= os.environ.get("MONGO_PASSWORD")
-DB_HOST= os.environ.get("MONGO_URL")
-SECRET= os.environ.get("SECRET_KEY")
+DB_NAME = os.environ.get("DB_NAME")
+DB_USER = os.environ.get("MONGO_USERNAME")
+DB_PASSWORD = os.environ.get("MONGO_PASSWORD")
+DB_HOST = os.environ.get("MONGO_URL")
+SECRET = os.environ.get("SECRET_KEY")
+INTELLIJ = "true"
+
 
 class ConfigFlask(object):
     DEBUG = True
@@ -20,3 +25,13 @@ class ConfigFlask(object):
     }
     MONGO_URI = f"mongodb+srv://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}?retryWrites=true&w=majority"
     SECRET_KEY = SECRET
+
+
+USER = [
+    {"endpoint": USER_ROUTE, "view_func": f"{USER_VIEW}.add_user", "methods": ['POST']}
+]
+
+routes = [
+    {"endpoint": "/", "view_func": "UserView.render", "methods": ["GET"]},
+    *USER
+]
