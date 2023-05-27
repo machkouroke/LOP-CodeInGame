e53e221bb9797d4dd5a029f20fce1d6bba096568
MorelK1 <kouhossounon.vianney@gmail.com>
2023-05-28 00:43:58 +0100

    `refactor` remove login endpoint to route login

--- api/app.py
+++ api/app.py
@@ -1,12 +1,8 @@
-from typing import Annotated
-
 import uvicorn
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from api.dependencies.user import get_current_user
-from api.routes import route_users, route_exos
-from api.src.models.User import User
+from api.routes import route_users, route_exos, route_login
 
 app = FastAPI()
 
@@ -16,13 +12,9 @@ def home():
     return {"Hello": "World"}
 
 
-@app.get('/me', summary='Get details of currently logged in user')
-async def get_me(user=Depends(get_current_user)):
-    return user.to_json()
-
-
-app.include_router(route_users.router, )
+app.include_router(route_users.router, prefix='/users')
 app.include_router(route_exos.router, prefix='/exos')
+app.include_router(route_login.router, )
 
 app.add_middleware(
     CORSMiddleware,
---
+++ api/routes/route_login.py
@@ -0,0 +1,38 @@
+from fastapi import APIRouter, Depends, HTTPException, status
+from werkzeug.exceptions import BadRequest
+
+from api import verify_password
+from api.dependencies.db import get_db
+from api.dependencies.user import get_current_user
+from api.src.authentication.jwt_encode import encode_auth_token
+from api.src.models.User import UserAuth, User
+
+router = APIRouter()
+
+
+@router.post('/login')
+async def login(form_data: UserAuth, db=Depends(get_db)):
+    if not form_data.mail or not form_data.password:
+        raise BadRequest('mail or password is empty')
+    user = User.find_one_or_404(database=db, mask={"mail": form_data.mail})
+    if user is None:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Email incorrect"
+        )
+    if not verify_password(form_data.password, user.password):
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Incorrect email or password"
+        )
+    if auth_token := encode_auth_token(user.id):
+        return {
+            'success': True,
+            'message': 'Successfully logged in.',
+            'auth_token': auth_token
+        }
+
+
+@router.get('/status', summary='Get details of currently logged in user')
+async def get_status(user=Depends(get_current_user)):
+    return user
