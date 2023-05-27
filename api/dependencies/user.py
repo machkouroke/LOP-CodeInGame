d7b723ea4ed527acc44f1ce2dbe9eaea6b709531
MorelK1 <kouhossounon.vianney@gmail.com>
2023-05-27 13:37:34 +0100

    `feat` get user connected

---
+++ api/dependencies/user.py
@@ -0,0 +1,32 @@
+from typing import Union, Any, Annotated
+
+import jwt
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from bson import ObjectId
+
+from api.dependencies.db import get_db
+from api.src.authentication.jwt_encode import decode_auth_token
+from api.src.models.User import User
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl='/login')
+
+
+def get_current_user(token: str = Depends(oauth2_scheme), db=Depends(get_db)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+    )
+    print(f'token:{token}')
+    try:
+        payload = ObjectId(decode_auth_token(token))
+        print(f'{payload}, {type(payload)}')
+        if payload is None:
+            raise credentials_exception
+    except jwt.PyJWTError:
+        raise credentials_exception
+    user = User.find_one_or_404(database=db, mask={'_id': payload})
+    if user is None:
+        raise credentials_exception
+
+    return user
