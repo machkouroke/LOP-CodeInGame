39407796455f195281499f072fe91ab2209cfc70
machkouroke <machkour20.mo1@gmail.com>
2023-05-30 20:47:05 +0100

    `feat` Modification  de la requêtes de modification en requêtes PATCH puis ajout de l'id dans l'url

--- api/routes/route_exos.py
+++ api/routes/route_exos.py
@@ -4,11 +4,13 @@ from fastapi import APIRouter, Depends, HTTPException, status
 
 from api.dependencies.db import get_db
 from api.dependencies.user import get_current_user
-from api.src.models.Exercise import Exercise, ExoToAdd, ExoStart
+from api.src.models.DTO import ExoStart
+from api.src.models.Exercise import Exercise, ExoToAdd
 from api.src.models.User import Teacher
 from api.src.models.objectid import PydanticObjectId
 
 from datetime import timezone
+
 router = APIRouter()
 
 
@@ -35,17 +37,18 @@ def add_exercise(exo_to_add: ExoToAdd, user=Depends(get_current_user), db=Depend
     }
 
 
-@router.post('/start')
-def start_competition(start: ExoStart, user=Depends(get_current_user), db=Depends(get_db)):
+@router.patch('/{id_exo}/start')
+def start_competition(id_exo: str, start: ExoStart, user=Depends(get_current_user), db=Depends(get_db)):
     if user.Type != 'teacher':
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
-            detail=f"Vous n'êtes pas autorisé à faire cette operation")
-    exo = Exercise.find_one_or_404(database=db, mask={'_id': PydanticObjectId(start.id)})
+            detail="Vous n'êtes pas autorisé à faire cette operation",
+        )
+    exo = Exercise.find_one_or_404(database=db, mask={'_id': PydanticObjectId(id_exo)})
     if exo is None:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
-            detail=f"L'exercice n'existe pas"
+            detail="L'exercice n'existe pas",
         )
     exo.update(data={'start': start.start, 'end': start.end})
     return {
@@ -54,10 +57,7 @@ def start_competition(start: ExoStart, user=Depends(get_current_user), db=Depend
     }
 
 
-@router.get('/{id}')
-def get_exercice(id: str, db=Depends(get_db)):
-    # id = PydanticObjectId(id)
-    # return {"id": id}
-    exo = Exercise.find_one_or_404(database=db, mask={'_id': PydanticObjectId(id)})
-    data = exo.to_json(to_exclude=set('owner_id'))
-    return data
+@router.get('/{id_exo}')
+def get_exercice(id_exo: str, db=Depends(get_db)):
+    exo = Exercise.find_one_or_404(database=db, mask={'_id': PydanticObjectId(id_exo)})
+    return exo.to_json(to_exclude=set('owner_id'))
