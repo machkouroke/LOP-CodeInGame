f3502ff9518e29c1668f46a3651c1924655f3fe6
machkouroke <machkour20.mo1@gmail.com>
2023-06-03 17:02:41 +0100

    `fix` Restriction du menu créateur au utilisateur éléve

--- ui/src/views/dahsboard/default/index.tsx
+++ ui/src/views/dahsboard/default/index.tsx
@@ -17,8 +17,6 @@ import useColorIcon from "../../../hooks/useColorIcon";
 import {getUserInfo} from "../../../slices/selector";
 
 
-
-
 export default function MainDashboard() {
     const history = useHistory();
     const user = useSelector(getUserInfo);
@@ -26,6 +24,7 @@ export default function MainDashboard() {
     const dispatch = useDispatch();
     // Chakra Color Mode
     const brandColor = useColorModeValue('brand.500', 'white');
+    const creatorIconColor = useColorIcon("#a60000");
     const boxBg = useColorModeValue('secondaryGray.300', 'whiteAlpha.100');
 
     return (
@@ -37,25 +36,26 @@ export default function MainDashboard() {
                 </Text>
                 <div></div>
             </SimpleGrid>
-            <SimpleGrid columns={{base: 1, md: 3}} gap='20px' mb='20px'>
-                <div></div>
-                <MiniStatistics
-                    onClick={() => {
-                        dispatch(next({currentPath: "/", history: history, nextPath: "/dashboard/creator"}));
-                    }}
+            {user.role === "teacher" &&
+                <SimpleGrid columns={{base: 1, md: 3}} gap='20px' mb='20px'>
+                    <div></div>
+                    <MiniStatistics
+                        onClick={() => {
+                            dispatch(next({currentPath: "/", history: history, nextPath: "/dashboard/creator"}));
+                        }}
 
-                    startContent={
-                        <IconBox
-                            w='56px'
-                            h='56px'
-                            bg={useColorIcon("#a60000")}
-                            icon={<Icon w='32px' h='32px' as={BiPaint} color={"white"}/>}
-                        />
-                    }
-                    value='Menu pour créateur'
-                />
-                <div></div>
-            </SimpleGrid>
+                        startContent={
+                            <IconBox
+                                w='56px'
+                                h='56px'
+                                bg={creatorIconColor}
+                                icon={<Icon w='32px' h='32px' as={BiPaint} color={"white"}/>}
+                            />
+                        }
+                        value='Menu pour créateur'
+                    />
+                    <div></div>
+                </SimpleGrid>}
             <SimpleGrid columns={{base: 1, md: 2}} gap='20px' mb='20px'>
 
                 <MiniStatistics
@@ -63,14 +63,14 @@ export default function MainDashboard() {
                         <IconBox
                             w='56px'
                             h='56px'
-                            bg={ useColorIcon("#75f542")}
+                            bg={useColorIcon("#75f542")}
                             icon={<Icon w='32px' h='32px'
                                         as={MdBarChart}
                                         color={"white"}/>}
                         />
                     }
                     name='Expérience'
-                    value={ `${user.experience} XP`}
+                    value={`${user.experience} XP`}
                 />
 
                 <MiniStatistics
@@ -78,7 +78,7 @@ export default function MainDashboard() {
                         <IconBox
                             w='56px'
                             h='56px'
-                            bg={ useColorIcon("#42bff5")}
+                            bg={useColorIcon("#42bff5")}
                             icon={<Icon w='28px' h='28px' as={TbMilitaryRank} color='white'/>}
                         />
                     }
@@ -94,7 +94,7 @@ export default function MainDashboard() {
                         <IconBox
                             w='56px'
                             h='56px'
-                            bg={ useColorIcon("#c842f5")}
+                            bg={useColorIcon("#c842f5")}
                             icon={<Icon w='32px' h='32px' as={MdNumbers} color={brandColor}/>}
                         />
                     }
@@ -111,7 +111,7 @@ export default function MainDashboard() {
                             <IconBox
                                 w='56px'
                                 h='56px'
-                                bg={ useColorIcon("#0dbdbd")}
+                                bg={useColorIcon("#0dbdbd")}
                                 icon={<Icon w='32px' h='32px' as={BsFillPlayFill} color={brandColor}/>}
                             />
                         }
