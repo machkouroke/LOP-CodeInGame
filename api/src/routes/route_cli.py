395c708576ba2f6b8cbdf7275b4e824bd954f354
machkouroke <machkour20.mo1@gmail.com>
2023-06-05 04:31:54 +0100

    `feat` Ajout de la fonction lopsubmit

--- api/src/routes/route_cli.py
+++ api/src/routes/route_cli.py
@@ -26,7 +26,7 @@ async def lop_submit(websocket: WebSocket,
     await websocket.accept()
     temp_path = Path("temp/")
     temp_path.mkdir(parents=True, exist_ok=True)
-    file_name = f"archive_{user.id}_{exercise_id}.zip"
+    file_name = f"archive_{user.id}_{exercise_id}.zip" if kind == "solve" else f"archive_{exercise_id}.zip"
     file_path = temp_path / file_name
     try:
         with open(file_path, "ab") as f:
@@ -50,12 +50,9 @@ async def lopgenerate(exercise_id: str, user: UserModel = Depends(get_current_us
 
 
 @router.get("/lopdownload")
-async def lop_download(competition_id: str,
-                       user_id: str,
-                       kind: str, bucket=Depends(get_bucket),
-                       user: UserModel = Depends(get_current_user_object)):
-    file_name = f"archive_{user_id}_{competition_id}.zip"
-    dest_path = "LOPInGame/solve/" if kind == "solve" else "LOPInGame/template/"
+async def lop_download(exercise_id: str, bucket=Depends(get_bucket)):
+    file_name = f"archive_{exercise_id}.zip"
+    dest_path = "LOPInGame/template/"
 
     return {
         "status": "success",
